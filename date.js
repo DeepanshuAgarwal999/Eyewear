9d14eac67396c0a2cbb754b50dce4e19b3b9c228
deepanshu agarwal <95143999+DeepanshuAgarwal999@users.noreply.github.com>
2023-06-20 17:23:27 +0530

    Update date.js

--- date.js
+++ date.js
@@ -1,10 +1,28 @@
-const hrs=document.getElementById("hrs")
-const mins=document.getElementById("mins")
-const sec=document.getElementById("sec")
-setInterval(()=>{
-  let currentTime=new Date();
-  hrs.innerHTML=(currentTime.getHours()<10?"0":"")+currentTime.getHours();
-  mins.innerHTML=(currentTime.getMinutes()<10?"0":"")+currentTime.getMinutes();
-  sec.innerHTML=(currentTime.getSeconds()<10?"0":"")+currentTime.getSeconds();
 
-},1000)
+
+const days = document.getElementById('days').innerHTML;
+const hours = document.getElementById('hours').innerHTML;
+const minutes = document.getElementById('minutes').innerHTML;
+const seconds = document.getElementById('seconds').innerHTML;
+
+let time = Number(days) * 24 * 60 * 60 + Number(hours)* 60 * 60 + Number(minutes) * 60 + Number(seconds);
+
+
+setInterval(() => {
+    let days = Math.floor(time / (24 * 60 * 60));
+    // console.log(days);
+    document.getElementById('days').textContent = days.toString();
+
+    let hours = Math.floor((time - (days * 24 * 60 * 60)) / (60 * 60));
+    // console.log(hours);
+    document.getElementById('hours').textContent = hours.toString();
+
+    let minutes = Math.floor((time - (days * 24 * 60 * 60) - (hours * 60 * 60)) / 60);
+    // console.log(minutes);
+    document.getElementById('minutes').textContent = minutes.toString();
+
+    let seconds = Math.floor(time - (days * 24 * 60 * 60) - (hours * 60 * 60) - minutes * 60);
+    // console.log(seconds);
+    document.getElementById('seconds').textContent = seconds.toString();
+    time--;
+}, 1000)
